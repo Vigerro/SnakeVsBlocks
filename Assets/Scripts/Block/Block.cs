04be11d4032587b800b5ba5502460c66c73b26e3
Vigerro <d3vvictor@gmail.com>
2021-12-16 19:59:08 +0300

    Project version 1.1

--- Assets/Scripts/Block/Block.cs
+++ Assets/Scripts/Block/Block.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Block : MonoBehaviour
 {
     [SerializeField] private Vector2Int _destroyPriceRange;
+    [Header("Colors by ascending block value")]
+    [SerializeField] private Color[] _colors;
 
     private int _destroyPrice;
     private int _filling = 0;
+    private SpriteRenderer _render;
 
     private int _leftToFill => _destroyPrice - _filling;
 
@@ -16,8 +20,11 @@ public class Block : MonoBehaviour
 
     private void Start()
     {
+        _render = GetComponent<SpriteRenderer>();
+
         _destroyPrice = Random.Range(_destroyPriceRange.x, _destroyPriceRange.y + 1);
         FillingUpdated?.Invoke(_leftToFill);
+        SetColor();
     }
 
     public void Fill()
@@ -30,5 +37,36 @@ public class Block : MonoBehaviour
         }
 
         FillingUpdated?.Invoke(_leftToFill);
+    }
+    
+    private void SetColor()
+    {
+        if(_destroyPrice <= 7)
+        {
+            _render.color = _colors[0];
+        }
+        else if(_destroyPrice <= 14){
+            _render.color = _colors[1];
+        }
+        else if (_destroyPrice <= 21)
+        {
+            _render.color = _colors[2];
+        }
+        else if (_destroyPrice <= 28)
+        {
+            _render.color = _colors[3];
+        }
+        else if (_destroyPrice <= 35)
+        {
+            _render.color = _colors[4];
+        }
+        else if (_destroyPrice <= 42)
+        {
+            _render.color = _colors[5];
+        }
+        else if (_destroyPrice <= 50)
+        {
+            _render.color = _colors[6];
+        }
     }
 }
--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -12,26 +12,32 @@ public class Spawner : MonoBehaviour
     [Header("Spawn templates")]
     [SerializeField] private Block _blockTemplate;
     [SerializeField] private Wall _wallTemplate;
+    [SerializeField] private Bonus _bonusTemplate;
     [Header("Chance in %")]
     [SerializeField] private int _blockSpawnChance;
     [SerializeField] private int _wallSpawnChance;
+    [SerializeField] private int _bonusSpawnChance;
 
     private BlockSpawnPosition[] _blockSpawnPositions;
     private WallSpawnPosition[] _wallSpawnPositions;
+    private BonusSpawnPosition[] _bonusSpawnPositions;
 
     private void Start()
     {
         _blockSpawnPositions = GetComponentsInChildren<BlockSpawnPosition>();
         _wallSpawnPositions = GetComponentsInChildren<WallSpawnPosition>();
+        _bonusSpawnPositions = GetComponentsInChildren<BonusSpawnPosition>();
 
         for (int i = 0; i < _repeatCount; i++)
         {
             MoveSpawner(_distanceBetweenFullLines);
             GenerateRandomLine(_wallSpawnPositions, _wallTemplate.gameObject, _wallSpawnChance, _distanceBetweenFullLines / 2f, _distanceBetweenFullLines / 4f);
+            GenerateRandomLine(_bonusSpawnPositions, _bonusTemplate.gameObject, _bonusSpawnChance);
             GenerateFullLine(_blockSpawnPositions, _blockTemplate.gameObject);
             MoveSpawner(_distanceBetweenRandomLines);
             GenerateRandomLine(_wallSpawnPositions, _wallTemplate.gameObject, _wallSpawnChance, _distanceBetweenRandomLines / 2f, _distanceBetweenRandomLines / 4f);
             GenerateRandomLine(_blockSpawnPositions, _blockTemplate.gameObject, _blockSpawnChance);
+            GenerateRandomLine(_bonusSpawnPositions, _bonusTemplate.gameObject, _bonusSpawnChance);
         }
     }
 
